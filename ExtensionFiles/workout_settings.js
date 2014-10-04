39bd4f1c5fffa27d245112d05666c42055145a7c
SamuelLMoney <sammassari13@live.com>
2014-10-04 19:48:58 -0400

    added localstorage for setttings page

--- ExtensionFiles/workout_settings.js
+++ ExtensionFiles/workout_settings.js
@@ -93,7 +93,7 @@ function checkSelect(){
 	}
 }
 
-document.getElementById("pushupsCheck").onchange=function(){
+function pushupsVarListing(){
 	if(document.getElementById("pushupsCheck").checked&&!pushupCreated){
 		pushupCreated=true;
 		var length=pushupVar.length;
@@ -126,7 +126,7 @@ document.getElementById("pushupsCheck").onchange=function(){
 	}
 };
 
-document.getElementById("pullupsCheck").onchange=function(){
+function pullupsVarListing(){
 	if(document.getElementById("pullupsCheck").checked&&!pullupCreated){
 		pullupCreated=true;
 		var length=pullupVar.length;
@@ -159,7 +159,7 @@ document.getElementById("pullupsCheck").onchange=function(){
 	}
 };
 
-document.getElementById("squatsCheck").onchange=function(){
+function squatsVarListing(){
 	if(document.getElementById("squatsCheck").checked&&!squatCreated){
 		squatCreated=true;
 		var length=squatVar.length;
@@ -192,7 +192,7 @@ document.getElementById("squatsCheck").onchange=function(){
 	}
 };
 
-document.getElementById("bridgesCheck").onchange=function(){
+function bridgesVarListing(){
 	if(document.getElementById("bridgesCheck").checked&&!bridgeCreated){
 		bridgeCreated=true;
 		var length=bridgeVar.length;
@@ -225,7 +225,7 @@ document.getElementById("bridgesCheck").onchange=function(){
 	}
 };
 
-document.getElementById("legraisesCheck").onchange=function(){
+function legRaisesVarListing(){
 	if(document.getElementById("legraisesCheck").checked&&!legraiseCreated){
 		legraiseCreated=true;
 		length=pushupVar.length;
@@ -258,7 +258,7 @@ document.getElementById("legraisesCheck").onchange=function(){
 	}
 };
 
-document.getElementById("hspushupsCheck").onchange=function(){
+function hsPushupsVarListing(){
 	if(document.getElementById("hspushupsCheck").checked&&!handstandpushupCreated){
 		handstandpushupCreated=true;
 		var length=handstandPushupsVar.length;
@@ -293,62 +293,71 @@ document.getElementById("hspushupsCheck").onchange=function(){
 
 // Saves options to chrome.storage
 function save_options() {
-	var doPushups=document.getElementById("pushupsCheck").checked;
-	var doPullups=document.getElementById("pullupsCheck").checked;
-	var doSquats=document.getElementById("squatsCheck").checked;
-	var doLegraises=document.getElementById('legraisesCheck').checked;
-	var doBridges=document.getElementById('bridgesCheck').checked;
-	var doHSPushups=document.getElementById('hspushupsCheck').checked;
-	
-	var timingInterval=document.getElementById("timingInterval").selectedIndex;
-	
-	chrome.storage.sync.set({
-		doPushups:doPushups,
-		doPullups:doPullups,
-		doSquats:doSquats,
-		doLegraises:doLegraises,
-		doBridges:doBridges,
-		doHSPushups:doHSPushups,
-		timingInterval:timingInterval
-		}, function(){
-			//update status to let user know options were saved
+	if(typeof(Storage)!=="undefined"){
+		var doPushups=document.getElementById("pushupsCheck").checked;
+		var doPullups=document.getElementById("pullupsCheck").checked;
+		var doSquats=document.getElementById("squatsCheck").checked;
+		var doLegraises=document.getElementById('legraisesCheck').checked;
+		var doBridges=document.getElementById('bridgesCheck').checked;
+		var doHSPushups=document.getElementById('hspushupsCheck').checked;
+		
+		var timingInterval=document.getElementById("timingInterval").selectedIndex;
+		
+		localStorage.setItem("doPushups",doPushups);
+		localStorage.setItem("doPullups",doPullups);
+		localStorage.setItem("doSquats",doSquats);
+		localStorage.setItem("doLegraises",doLegraises);
+		localStorage.setItem("doBridges",doBridges);
+		localStorage.setItem("doHSPushups",doHSPushups);
+		localStorage.setItem("timingInterval",timingInterval)
+		document.getElementById("test").innerHTML=localStorage.doPushups;
+		//update status to let user know options were saved
 			var status=document.getElementById('status');
 			status.textContent='Options saved.';
 			setTimeout(function(){
 				status.textContent='';
 		},750);
-	});
+	}else{
+		document.getElementById("test").innerHTML="nolocalstorage";
+	}
 	
 }
 	
 
-
+var doArray=["doPushups","doPullups","doSquats","doBridges","doHSPushups","doLegraises"];
+doArray.constructor;
+var checkArray=["pushupsCheck","pullupsCheck","squatsCheck","bridgesCheck","hspushupsCheck","legraisesCheck"];
+checkArray.constructor;
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
-  // Use default value color = 'red' and likesColor = true.
-  chrome.storage.sync.get({
-		doPushups: false,
-		doPullups: false,
-		doSquats: false,
-		doLegraises: false,
-		doBridges: false,
-		doHSPushups: false,
-		timingInterval: 0
-   }, function(items) {
-		document.getElementById("pushupsCheck").checked=items.doPushups;
-		document.getElementById("pullupsCheck").checked=items.doPullups;
-		document.getElementById("squatsCheck").checked=items.doSquats;
-		document.getElementById('legraisesCheck').checked=items.doLegraises;
-		document.getElementById('bridgesCheck').checked=items.doBridges;
-		document.getElementById('hspushupsCheck').checked=items.doHSPushups;
+		for(i=0;i<doArray.length;i++){
+			if(localStorage.getItem(doArray[i])=="true"){
+				document.getElementById(checkArray[i]).checked=true;
+			}
+		}
+		document.getElementById("timingInterval").selectedIndex=localStorage.getItem("timingInterval");
 	
-		document.getElementById("timingInterval").selectedIndex=timingInterval;
-	});
 }
 
 document.addEventListener('DOMContentLoaded', restore_options);
 document.getElementById('save').addEventListener('click',
     save_options);
+//open workout listings on startup
+document.addEventListener('DOMContentLoaded', pushupsVarListing);
+document.addEventListener('DOMContentLoaded', pullupsVarListing);
+document.addEventListener('DOMContentLoaded', squatsVarListing);
+document.addEventListener('DOMContentLoaded', legRaisesVarListing);
+document.addEventListener('DOMContentLoaded', bridgesVarListing);
+document.addEventListener('DOMContentLoaded', hsPushupsVarListing);
+
+//modify listings whenever clicked
+document.getElementById("pushupsCheck").addEventListener('change', pushupsVarListing);
+document.getElementById("pullupsCheck").addEventListener('change', pullupsVarListing);
+document.getElementById("squatsCheck").addEventListener('change', squatsVarListing);
+document.getElementById("legraisesCheck").addEventListener('change', legRaisesVarListing);
+document.getElementById("bridgesCheck").addEventListener('change', bridgesVarListing);
+document.getElementById("hspushupsCheck").addEventListener('change', hsPushupsVarListing);
+
 document.body.addEventListener('click',checkSelect,true);
 document.getElementById('removeButton').addEventListener('click',removeExcludedSite);
