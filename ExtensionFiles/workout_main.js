3bb2baec4ec2a6ea549089ebad7694b6dd51c201
SamuelLMoney <sammassari13@live.com>
2014-10-05 00:19:59 -0400

    fixed functions and added listeners

--- ExtensionFiles/workout_main.js
+++ ExtensionFiles/workout_main.js
@@ -1,13 +1,4 @@
-/*
-  JavaScript repository for Coach crtr
-  All JavaScript functions are to be put in this file
-*/
-
-function success_Prompt() {
-			
-	<!-- Update user prompt -->
-	document.getElementById("userInstructions").innerHTML = "How difficult was the workout?";
-	var workouts= ["Pushups","Pullups","Squats","Bridges","Handstand Pushups","Leg Raises"];
+var workouts= ["Pushups","Pullups","Squats","Bridges","Handstand Pushups","Leg Raises"];
 workouts.constructor;
 
 
@@ -74,7 +65,11 @@ nameArray.constructor;
 var versionArray=["","","","","",""];
 versionArray.constructor;
 
+var indexUsed;
+
 function setRepExercise(){
+	document.getElementById("btnSucess").addEventListener("click", success_Prompt);
+	document.getElementById("btnFail").addEventListener("click", fail_Prompt);
 	var choiceMade=false;
 	for(i=0;i<6;i++){
 		if(localStorage.getItem(doArray[i])=="true"){
@@ -83,14 +78,30 @@ function setRepExercise(){
 	}
 	if(choiceMade==false){
 		document.getElementById("nochoice").innerHTML="Go make some selections on the Options Page!";
+		document.getElementById("nochoice").innerHTML="<h1 id=\"nochoice\" >Go make some selections on the <button id=\"btnOptions\" type=\button\ \">Options</button> Page!</h1>";
+		document.getElementById("btnOptions").addEventListener("click",openOptions);
 	}else{
 		while(localStorage.getItem(doArray[index])!="true"){
 			var index=randomIntFromInterval(0,5);
 		}
-		document.getElementById("exercise").innerHTML=localStorage.getItem(workouts[index])+" "+workouts[index];
-		document.getElementById("reps").innerHTML=repProgress[index];
+		document.getElementById("exercise").innerHTML=localStorage.getItem(workouts[index]);
+		if(index!=4){
+			document.getElementById("exercise").innerHTML=document.getElementById("exercise").innerHTML+" "+workouts[index];
+		}
+		if(!(localStorage.getItem(index))){
+			document.getElementById("reps").innerHTML=repProgress[index];
+			localStorage.setItem(index,repProgress[index]);
+			indexUsed=index;
+		}else{
+			document.getElementById("reps").innerHTML=localStorage.getItem(index);
+			indexUsed=index;
+		}
 	}
 	
+}
+
+function openOptions(){
+	window.open("workout_settings.html");
 }
 
 function randomIntFromInterval(min,max)
@@ -98,8 +109,14 @@ function randomIntFromInterval(min,max)
     return Math.floor(Math.random()*(max-min+1)+min);
 }
 
-document.addEventListener('DOMContentLoaded',setRepExercise);
-	<!-- Remove existing buttons -->
+function success_Prompt() {
+			
+	<!-- Update user prompt -->
+	document.getElementById("userInstructions").innerHTML = "How difficult was the workout?";
+
+	document.getElementById("btnSucess").removeEventListener("click", success_Prompt);
+	document.getElementById("btnFail").removeEventListener("click", fail_Prompt);
+<!-- Remove existing buttons -->
 	element = document.getElementById("btnSucess");
 	element.parentNode.removeChild(element);
 	element = document.getElementById("btnFail");
@@ -108,14 +125,18 @@ document.addEventListener('DOMContentLoaded',setRepExercise);
 	<!-- Create the easy button-->
 	
 	divHTML = document.getElementById("div1").innerHTML;
-	divHTML = divHTML.concat("<button id=\"btnEasy\" type=\button\ onclick=\"setEasy()\">Easy</button>");
+	divHTML = divHTML.concat("<button id=\"btnEasy\" type=\button\ \">Easy</button>");
 	document.getElementById("div1").innerHTML = divHTML;
+	
 	
 	<!-- Create the hard button-->
 	
 	divHTML = document.getElementById("div1").innerHTML;
-	divHTML = divHTML.concat("<button id=\"btnHard\" type=\button\ onclick=\"setHard()\">Hard</button>");
+	divHTML = divHTML.concat("<button id=\"btnHard\" type=\button\ \">Hard</button>");
 	document.getElementById("div1").innerHTML = divHTML;
+	
+	document.getElementById("btnEasy").addEventListener("click",setEasy);
+	document.getElementById("btnHard").addEventListener("click",setHard);
 }
 
 function fail_Prompt() {
@@ -123,6 +144,8 @@ function fail_Prompt() {
 	<!-- Update user instruction text -->
 	document.getElementById("userInstructions").innerHTML = "How many reps did you complete?";
 	
+	document.getElementById("btnSucess").removeEventListener("click", success_Prompt);
+	document.getElementById("btnFail").removeEventListener("click", fail_Prompt);
 	<!-- Remove Existing Buttons -->
 	element = document.getElementById("btnSucess");
 	element.parentNode.removeChild(element);
@@ -138,373 +161,59 @@ function fail_Prompt() {
 	<!-- Create the log button-->
 	
 	divHTML = document.getElementById("div1").innerHTML;
-	divHTML = divHTML.concat("<button id=\"btnLog\" type=\button\ onClick=\"repTotal()\">Log</button>");
+	divHTML = divHTML.concat("<button id=\"btnLog\" type=\button\ \">Log</button>");
 	document.getElementById("div1").innerHTML = divHTML;
+	document.getElementById("btnLog").addEventListener("click",adjustFailedReps);
 				
 }
 
+function adjustFailedReps(){
+	var newReps=Number(document.getElementById("repIn").value)+randomIntFromInterval(1,3);
+	localStorage.setItem(indexUsed,newReps);
+	
+	<!-- Update user instruction text -->
+	document.getElementById("userInstructions").innerHTML = "Don't worry, it will be a little easier next time! Feel free to close this tab now.";
+	
+	document.getElementById("btnLog").removeEventListener("click",adjustFailedReps);
+	<!-- Remove Existing Buttons -->
+	element = document.getElementById("btnLog");
+	element.parentNode.removeChild(element);
+	element = document.getElementById("repIn");
+	element.parentNode.removeChild(element);
+}
+
 function setEasy() {
-			
+			<!-- Update user instruction text -->
+	document.getElementById("userInstructions").innerHTML = "Easy? It'll be harder next time! Feel free to close this tab now.";
+	var newReps=Number(localStorage.getItem(indexUsed))+randomIntFromInterval(1,3);
+	localStorage.setItem(indexUsed,newReps);
+	
+	document.getElementById("btnEasy").removeEventListener("click",setEasy);
+	document.getElementById("btnHard").removeEventListener("click",setEasy);
+	<!-- Remove Existing Buttons -->
+	element = document.getElementById("btnEasy");
+	element.parentNode.removeChild(element);
+	element = document.getElementById("btnHard");
+	element.parentNode.removeChild(element);
 }
 			
 function setHard() {
-
+		<!-- Update user instruction text -->
+	document.getElementById("userInstructions").innerHTML = "Hard? But you completed it! It'll be easy next time! Feel free to close this tab now.";
+	var newReps=Number(localStorage.getItem(indexUsed))+randomIntFromInterval(1,3);
+	localStorage.setItem(indexUsed,newReps);
+	
+	document.getElementById("btnEasy").removeEventListener("click",setEasy);
+	document.getElementById("btnHard").removeEventListener("click",setEasy);
+	<!-- Remove Existing Buttons -->
+	element = document.getElementById("btnEasy");
+	element.parentNode.removeChild(element);
+	element = document.getElementById("btnHard");
+	element.parentNode.removeChild(element);
 }
 		
 function repTotal() {
 	uReps = document.getElementById("repIn").value;
 }
 
-var workouts= ["Pushups","Pullups","Leg Raises","Bridges","Squats","Handstand Pushups"];
-workouts.constructor;
-
-
-var pushupCreated=false;
-var pushupVar= ["Wall","Inclined","Kneeling","Half","Full","Close","Uneven","1-Arm Half","Lever","1-Arm"];
-var pushupBeginRep=[10,10,10,8,5,5,5,5,5];
-var pushupProgressRep=[50,40,30,25,20,20,20,20,20,100];
-pushupVar.constructor;
-pushupBeginRep.constructor;
-pushupProgressRep.constructor;
-
-var pullupCreated=false;
-var pullupVar= ["Vertical","Horizontal","Jack Knife","Half","Full","Uneven","1-Arm Half","Assisted 1-Arm","1-Arm"];
-var pullupBeginRep=[10,10,10,8,5,5,5,4,3,1];
-var pullupProgressRep=[40,30,20,15,10,10,9,8,7,6];
-pullupVar.constructor;
-pullupBeginRep.constructor;
-pullupProgressRep.constructor;
-
-var legraiseCreated=false;
-var legraisesVar= ["Knee Tucks","Flat Knee Raises","Flat Bent Leg Raises","Flat Frog Raises","Flat Straight Leg Raises","Hanging Knee Raises","Hanging Bent Leg Raises", "Hanging Frog Raises", "Partial Straight Leg Raises","Hanging Straight Leg Raises"];
-var legraisesBeginRep=[10,10,10,8,5,5,5,5,5,5];
-var legraisesProgressRep=[40,35,30,25,20,15,15,15,15,30];
-legraisesVar.constructor;
-legraisesBeginRep.constructor;
-legraisesProgressRep.constructor;
-
-var squatCreated=false;
-var squatVar=["Shoulderstand","Jackknife","Supported","Half","Full","Close","Uneven","Half 1-Legged","Assisted 1-Legged","1-Legged"];
-var squatBeginRep=[10,10,10,8,5,5,5,5,5,5];
-var squatProgressRep=[50,40,30,50,30,20,20,20,20,50];
-squatVar.constructor;
-squatBeginRep.constructor;
-squatProgressRep.constructor;
-
-var bridgeCreated=false;
-var bridgeVar=["Short","Straight","Angled","Head","Half","Full","Wall Walking (Down)","Wall Walking (Up)","Closing","Stand-to-Stand"];
-var bridgeBeginRep=[10,10,8,8,8,6,3,2,1,1];
-var bridgeProgressRep=[50,40,30,25,20,15,10,8,6,10];
-bridgeVar.constructor;
-bridgeBeginRep.constructor;
-bridgeProgressRep.constructor;
-
-var handstandpushupCreated=false;
-var handstandPushupsVar=["Wall Headstands","Crow Stands","Wall Handstands","Half Handstand PushUps","Handstand PushUps","Close Handstand PushUps","Uneven Handstand PushUps","Half 1-Arm Handstand PushUps","Lever Handstand PushUps","1-Arm Handstand PushUps"];
-var handstandPushupsBeginRep=[30,10,30,5,5,5,5,4,3,1];
-var handstandPushupsProgressRep=[120,60,120,20,15,12,10,8,6,5]
-handstandPushupsVar.constructor;
-handstandPushupsBeginRep.constructor;
-handstandPushupsProgressRep.constructor;
-
-
-//progress vars
-var pushupProgress=[0,10];
-pushupProgress.constructor;
-var pullupProgress=[0,10];
-pullupProgress.constructor;
-var squatProgress=[0,10];
-squatProgress.constructor;
-var legraiseProgress=[0,10];
-legraiseProgress.constructor;
-var bridgeProgress=[0,10];
-bridgeProgress.constructor;
-var hspushupProgress=[0,30];
-hspushupProgress.constructor;
-
-function addExcludedSite(){
-		var site=document.getElementById("excludedSite").value;
-		var select=document.getElementById("siteSelect");
-		var option=document.createElement("option");
-		option.text=site;
-		select.add(option);
-		document.getElementById("excludedSite").value="";
-	}
-	
-function removeExcludedSite(){
-	var select=document.getElementById("siteSelect");
-	var index=select.selectedIndex;
-	document.getElementById("test").innerHTML=index;
-	if(index!=-1){
-		select.remove(index);
-	}
-}
-
-function checkSelect(){
-	var select=document.getElementById("siteSelect");
-	var index=select.selectedIndex;
-	if(index==-1){
-		document.getElementById("removeButton").disabled=true;
-	}else{
-		document.getElementById("removeButton").disabled=false;
-	}
-}
-
-document.getElementById("pushupsCheck").onchange=function(){
-	if(document.getElementById("pushupsCheck").checked&&!pushupCreated){
-		pushupCreated=true;
-		var length=pushupVar.length;
-		for(i=0; i<length;i++){
-			currentWorkout=pushupVar[i].toString();
-			var radio=document.createElement("INPUT");
-			radio.setAttribute("type","radio");
-			radio.setAttribute("name","pushups");
-			radio.setAttribute("id",currentWorkout);
-			radio.value=currentWorkout;
-
-			var newDiv=document.createElement('div');
-			newDiv.id=currentWorkout;
-			newDiv.name="pushups";
-				
-			var span=document.createElement('span');
-			span.innerHTML=currentWorkout;
-	
-			newDiv.appendChild(radio);
-			newDiv.appendChild(span);
-			
-			document.getElementById("pushupsRadio").appendChild(newDiv);
-			
-			lastWorkout=currentWorkout;
-		}
-	}else if(!document.getElementById("pushupsCheck").checked){
-		document.getElementById("pushupsRadio").style.display="none";
-	}else{
-		document.getElementById("pushupsRadio").style.display="inline";
-	}
-};
-
-document.getElementById("pullupsCheck").onchange=function(){
-	if(document.getElementById("pullupsCheck").checked&&!pullupCreated){
-		pullupCreated=true;
-		var length=pullupVar.length;
-		for(i=0; i<length;i++){
-			currentWorkout=pullupVar[i].toString();
-			var radio=document.createElement("INPUT");
-			radio.setAttribute("type","radio");
-			radio.setAttribute("name","pullups");
-			radio.setAttribute("id",currentWorkout);
-			radio.value=currentWorkout;
-
-			var newDiv=document.createElement('div');
-			newDiv.id=currentWorkout;
-			newDiv.name="pullups";
-				
-			var span=document.createElement('span');
-			span.innerHTML=currentWorkout;
-	
-			newDiv.appendChild(radio);
-			newDiv.appendChild(span);
-			
-			document.getElementById("pullupsRadio").appendChild(newDiv);
-			
-			lastWorkout=currentWorkout;
-		}
-	}else if(!document.getElementById("pullupsCheck").checked){
-		document.getElementById("pullupsRadio").style.display="none";
-	}else{
-		document.getElementById("pullupsRadio").style.display="inline";
-	}
-};
-
-document.getElementById("squatsCheck").onchange=function(){
-	if(document.getElementById("squatsCheck").checked&&!squatCreated){
-		squatCreated=true;
-		var length=squatVar.length;
-		for(i=0; i<length;i++){
-			currentWorkout=squatVar[i].toString();
-			var radio=document.createElement("INPUT");
-			radio.setAttribute("type","radio");
-			radio.setAttribute("name","squats");
-			radio.setAttribute("id",currentWorkout);
-			radio.value=currentWorkout;
-
-			var newDiv=document.createElement('div');
-			newDiv.id=currentWorkout;
-			newDiv.name="squats";
-				
-			var span=document.createElement('span');
-			span.innerHTML=currentWorkout;
-	
-			newDiv.appendChild(radio);
-			newDiv.appendChild(span);
-			
-			document.getElementById("squatsRadio").appendChild(newDiv);
-			
-			lastWorkout=currentWorkout;
-		}
-	}else if(!document.getElementById("squatsCheck").checked){
-		document.getElementById("squatsRadio").style.display="none";
-	}else{
-		document.getElementById("squatsRadio").style.display="inline";
-	}
-};
-
-document.getElementById("bridgesCheck").onchange=function(){
-	if(document.getElementById("bridgesCheck").checked&&!bridgeCreated){
-		bridgeCreated=true;
-		var length=bridgeVar.length;
-		for(i=0; i<length;i++){
-			currentWorkout=pushupVar[i].toString();
-			var radio=document.createElement("INPUT");
-			radio.setAttribute("type","radio");
-			radio.setAttribute("name","bridges");
-			radio.setAttribute("id",currentWorkout);
-			radio.value=currentWorkout;
-
-			var newDiv=document.createElement('div');
-			newDiv.id=currentWorkout;
-			newDiv.name="bridges";
-				
-			var span=document.createElement('span');
-			span.innerHTML=currentWorkout;
-	
-			newDiv.appendChild(radio);
-			newDiv.appendChild(span);
-			
-			document.getElementById("bridgesRadio").appendChild(newDiv);
-			
-			lastWorkout=currentWorkout;
-		}
-	}else if(!document.getElementById("bridgesCheck").checked){
-		document.getElementById("bridgesRadio").style.display="none";
-	}else{
-		document.getElementById("bridgesRadio").style.display="inline";
-	}
-};
-
-document.getElementById("legraisesCheck").onchange=function(){
-	if(document.getElementById("legraisesCheck").checked&&!legraiseCreated){
-		legraiseCreated=true;
-		length=pushupVar.length;
-		for(i=0; i<length;i++){
-			currentWorkout=pushupVar[i].toString();
-			var radio=document.createElement("INPUT");
-			radio.setAttribute("type","radio");
-			radio.setAttribute("name","legraises");
-			radio.setAttribute("id",currentWorkout);
-			radio.value=currentWorkout;
-
-			var newDiv=document.createElement('div');
-			newDiv.id=currentWorkout;
-			newDiv.name="legraises";
-				
-			var span=document.createElement('span');
-			span.innerHTML=currentWorkout;
-	
-			newDiv.appendChild(radio);
-			newDiv.appendChild(span);
-			
-			document.getElementById("legraisesRadio").appendChild(newDiv);
-			
-			lastWorkout=currentWorkout;
-		}
-	}else if(!document.getElementById("legraisesCheck").checked){
-		document.getElementById("legraisesRadio").style.display="none";
-	}else{
-		document.getElementById("legraisesRadio").style.display="inline";
-	}
-};
-
-document.getElementById("hspushupsCheck").onchange=function(){
-	if(document.getElementById("hspushupsCheck").checked&&!handstandpushupCreated){
-		handstandpushupCreated=true;
-		var length=handstandPushupsVar.length;
-		for(i=0; i<length;i++){
-			currentWorkout=handstandPushupsVar[i].toString();
-			var radio=document.createElement("INPUT");
-			radio.setAttribute("type","radio");
-			radio.setAttribute("name","handstandpushups");
-			radio.setAttribute("id",currentWorkout);
-			radio.value=currentWorkout;
-
-			var newDiv=document.createElement('div');
-			newDiv.id=currentWorkout;
-			newDiv.name="handstandpushups";
-				
-			var span=document.createElement('span');
-			span.innerHTML=currentWorkout;
-	
-			newDiv.appendChild(radio);
-			newDiv.appendChild(span);
-			
-			document.getElementById("hspushupsRadio").appendChild(newDiv);
-			
-			lastWorkout=currentWorkout;
-		}
-	}else if(!document.getElementById("hspushupsCheck").checked){
-		document.getElementById("hspushupsRadio").style.display="none";
-	}else{
-		document.getElementById("hspushupsRadio").style.display="inline";
-	}
-};
-
-// Saves options to chrome.storage
-function save_options() {
-	var doPushups=document.getElementById("pushupsCheck").checked;
-	var doPullups=document.getElementById("pullupsCheck").checked;
-	var doSquats=document.getElementById("squatsCheck").checked;
-	var doLegraises=document.getElementById('legraisesCheck').checked;
-	var doBridges=document.getElementById('bridgesCheck').checked;
-	var doHSPushups=document.getElementById('hspushupsCheck').checked;
-	
-	var timingInterval=document.getElementById("timingInterval").selectedIndex;
-	
-	chrome.storage.sync.set({
-		doPushups:doPushups,
-		doPullups:doPullups,
-		doSquats:doSquats,
-		doLegraises:doLegraises,
-		doBridges:doBridges,
-		doHSPushups:doHSPushups,
-		timingInterval:timingInterval
-		}, function(){
-			//update status to let user know options were saved
-			var status=document.getElementById('status');
-			status.textContent='Options saved.';
-			setTimeout(function(){
-				status.textContent='';
-		},750);
-	});
-	
-}
-	
-
-
-// Restores select box and checkbox state using the preferences
-// stored in chrome.storage.
-function restore_options() {
-  // Use default value color = 'red' and likesColor = true.
-  chrome.storage.sync.get({
-		doPushups: false,
-		doPullups: false,
-		doSquats: false,
-		doLegraises: false,
-		doBridges: false,
-		doHSPushups: false,
-		timingInterval: 0
-   }, function(items) {
-		document.getElementById("pushupsCheck").checked=items.doPushups;
-		document.getElementById("pullupsCheck").checked=items.doPullups;
-		document.getElementById("squatsCheck").checked=items.doSquats;
-		document.getElementById('legraisesCheck').checked=items.doLegraises;
-		document.getElementById('bridgesCheck').checked=items.doBridges;
-		document.getElementById('hspushupsCheck').checked=items.doHSPushups;
-	
-		document.getElementById("timingInterval").selectedIndex=timingInterval;
-	});
-}
-
-document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click', save_options);
-document.body.addEventListener('click',checkSelect,true);
-document.getElementById('removeButton').addEventListener('click',removeExcludedSite);
+document.addEventListener('DOMContentLoaded',setRepExercise);
